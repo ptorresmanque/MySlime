c9267b9e94f9511c21e959585b8f9b6905ae7bfd
Patricio <ptorresmanque@gmail.com>
2018-12-20 07:14:57 -0300

    arreglos

--- Assets/scripts/GameStatus.cs
+++ Assets/scripts/GameStatus.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameStatus : MonoBehaviour {
 
     [SerializeField] TextMeshProUGUI scoreText;
     [SerializeField] TextMeshProUGUI pajarosText;
+    [SerializeField] TextMeshProUGUI scoreTextF;
+    [SerializeField] TextMeshProUGUI pajarosTextF;
     [SerializeField] Move ninja;
     [SerializeField] int currentScore = 100;
     [SerializeField] int pointsPerPajarraco;
     [SerializeField] int pointsPerDamege;
+    [SerializeField] GameObject finishUI;
+    [SerializeField] GameObject normalUI;
 
     private float timeBtwReduce;
     public float startTimeBtwReduceLife;
     private int pajarracos = 0;
+    private bool ciclo = true;
     
 
 
@@ -27,9 +33,14 @@ public class GameStatus : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (currentScore <= 0)
+		if (currentScore <= 0 && ciclo)
         {
             ninja.dying();
+            normalUI.SetActive(false);
+            scoreTextF.text = currentScore.ToString();
+            pajarosTextF.text = pajarracos.ToString();
+            finishUI.SetActive(true);
+            ciclo = false;
         }else if (timeBtwReduce <= 0)
         {
             timeBtwReduce = startTimeBtwReduceLife;
@@ -54,5 +65,10 @@ public class GameStatus : MonoBehaviour {
     {
         currentScore -= pointsPerDamege;
         scoreText.text = currentScore.ToString();
+    }
+
+    public void reStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
